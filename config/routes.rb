de35e8bbaf56af667fc21b1ed646ed64700506a7
Al-Amin <alaminkhanshakil@gmail.com>
2024-05-21 18:43:23 +0600

    Add user delete function

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'users#index'
 
-  resources :users, only: [:index, :show]
+  resources :users
   resources :posts
   # Defines the root path route ("/")
   # root "posts#index"
