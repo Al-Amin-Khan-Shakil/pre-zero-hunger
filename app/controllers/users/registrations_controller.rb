73e1582b1072c1dbe34a352407f99ba53266b7b9
Al-Amin <alaminkhanshakil@gmail.com>
2024-05-22 18:55:45 +0600

    Add & style user image

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,8 +5,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # Configure the permitted parameters for sign up and account update
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :email, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name age email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name age email avatar])
   end
 
   # Override the method for updating resource
